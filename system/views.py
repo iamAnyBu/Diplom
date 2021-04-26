2a41d3e750242abc22ce1143116c11ea763a7665
iamAnyBu <82719093+iamAnyBu@users.noreply.github.com>
2021-04-27 02:18:23 +0300

    Update views.py

--- system/views.py
+++ system/views.py
@@ -1,9 +1,44 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from system.forms import UserRegisterForm, UserLoginForm
+from django.contrib.auth import login as dj_login, logout as log
 
-# Create your views here.
 def base(request):
     return render(request, 'system/base.html')
 
+def base_login(request):
+    return render(request, 'system/base_login.html')
+
+def whoau(request):
+    return render(request, 'system/whoau.html')
+
+def empl(request):
+    return render(request,'system/empl.html')
+
+def shef(request):
+    return render(request,'system/shef.html')
+
 def login(request):
-    return render(request, 'system/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            dj_login(request, user)
+            return redirect('whoau')
+    else:
+        form = UserLoginForm()
+    return render(request, 'system/login.html', {"form": form})
+
+def logout(request):
+    log(request)
+    return redirect('base')
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    else:
+        form = UserRegisterForm()
+    return render(request, 'system/registration.html', {"form": form})

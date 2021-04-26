99cbfb9d64237b1109a46ac7484c486e8cc98fc9
iamAnyBu <82719093+iamAnyBu@users.noreply.github.com>
2021-04-27 02:17:04 +0300

    Update urls.py

--- system/urls.py
+++ system/urls.py
@@ -1,9 +1,15 @@
-from django.conf.urls import url
 from django.urls import path
 
 from system import views
 
+# АДРЕСА ПЕРЕХОДОВ К ФУНКЦИЯМ ВО views.py
 urlpatterns = [
     path('', views.base, name='base'),
+    path('base_login/',views.base_login, name='base_login'),
     path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('registration/', views.registration, name='registration'),
+    path('whoau/', views.whoau, name='whoau'),
+    path('shef/', views.shef, name='shef'),
+    path('empl/', views.empl, name='empl'),
 ]

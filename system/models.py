be5e0b21e34cac5ae7753b7c317e853a36edae78
iamAnyBu <82719093+iamAnyBu@users.noreply.github.com>
2021-05-29 16:03:30 +0300

    Update models.py

--- system/models.py
+++ system/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
-"""
-создание таблиц бд
-"""
+
 # Create your models here.
 class Client(models.Model):
     SecondName = models.CharField(max_length=50)
@@ -10,106 +8,141 @@ class Client(models.Model):
     LastName = models.CharField(max_length=50)
     Name = models.CharField(max_length=50)
     Address = models.CharField(max_length=70)
-    IdCategory = models.ForeignKey('ClientCategory', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.SecondName
 
 
-class ClientCategory(models.Model):
+class Department(models.Model):
     Name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.Name
+
+
+class Position(models.Model):
+    Name = models.CharField(max_length=50)
+    Department = models.ForeignKey('Department', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Name
 
 class Person(models.Model):
     SecondName = models.CharField(max_length=50)
     FirstName = models.CharField(max_length=50)
     LastName = models.CharField(max_length=50)
-    IdClient = models.ForeignKey("Client", on_delete=models.CASCADE)
-    IdPosition = models.ForeignKey('Position', on_delete=models.CASCADE)
+    Client = models.ForeignKey("Client", on_delete=models.CASCADE)
+    Position = models.ForeignKey('Position', on_delete=models.CASCADE)
 
-
-class Position(models.Model):
-    Name = models.CharField(max_length=50)
-    Id_category = models.ForeignKey('PositionCategory', on_delete=models.CASCADE)
-
-
-class PositionCategory(models.Model):
-    Name = models.CharField(max_length=50)
-
-
-class IndPers(models.Model):
-    IdPerson = models.ForeignKey('Person', on_delete=models.CASCADE)
-    IdIndPlan = models.ForeignKey('IndPlan', on_delete=models.CASCADE)
-
-
-class Result(models.Model):
-    IdIndPlan = models.ForeignKey('IndPlan', on_delete=models.CASCADE)
-    IdIndPers = models.ForeignKey('IndPers', on_delete=models.CASCADE)
-
-
-class IndPlan(models.Model):
-    Name = models.CharField(max_length=50)
-
-
-class Programm(models.Model):
-    Lection = models.IntegerField
-    Practice = models.IntegerField
-    Test = models.BooleanField
-    IdTopic = models.ForeignKey('Topic', on_delete=models.CASCADE)
-    IdIndPlan = models.ForeignKey('IndPlan', on_delete=models.CASCADE)
-
-
-class ResTopic(models.Model):
-    IdTopic = models.ForeignKey('Topic', on_delete=models.CASCADE)
-    IdResult = models.ForeignKey('Result', on_delete=models.CASCADE)
-
-
-class Topic(models.Model):
-    Name = models.CharField(max_length=50)
-    IdCourse = models.ForeignKey('Course', on_delete=models.CASCADE)
-    IdLevel = models.ForeignKey('Level', on_delete=models.CASCADE)
-
-
-class Level(models.Model):
-    Name = models.CharField(max_length=50)
-
-
-class Course(models.Model):
-    Name = models.CharField(max_length=50)
-    Duration = models.CharField(max_length=10)
-    Format = models.CharField(max_length=20)
+    def __str__(self):
+        return self.SecondName
 
 
 class Test(models.Model):
-    Date = models.DateField
-    Number = models.IntegerField
-    IdPerson = models.ForeignKey('Person', on_delete=models.CASCADE)
+    Data = models.DateTimeField()
+    Number = models.IntegerField()
+    Person = models.ForeignKey('Person', on_delete=models.CASCADE)
 
 
-class AnswerTest(models.Model):
-    IdTest = models.ForeignKey('Test', on_delete=models.CASCADE)
-    IdVarAnswer = models.ForeignKey('VarAnsTest', on_delete=models.CASCADE)
-    IdQuestion = models.ForeignKey('QuestionTest', on_delete=models.CASCADE)
+class TestAnswer(models.Model):
+    Test = models.ForeignKey('Test', on_delete=models.CASCADE)
+    VarAnswer = models.ForeignKey('VarAnswer', on_delete=models.CASCADE)
+    Question = models.ForeignKey('Question', on_delete=models.CASCADE)
+    Level = models.ForeignKey('Level', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Test
 
 
-class VarAnsTest(models.Model):
-    Name = models.CharField(max_length=50)
-    IdQuestion = models.ForeignKey('QuestionTest', on_delete=models.CASCADE)
+class VarAnswer(models.Model):
+    Name = models.CharField(max_length=80)
+    Question = models.ForeignKey('Question', on_delete=models.CASCADE)
+    Point = models.IntegerField()
+
+    def __str__(self):
+        return self.Name
 
 
-class QuestionTest(models.Model):
-    Question = models.CharField(max_length=50)
-    IdTopic = models.ForeignKey('Topic', on_delete=models.CASCADE)
+class Question(models.Model):
+    Wording = models.TextField()
+    Topic = models.ForeignKey('Topic', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Wording
 
 
 class Form(models.Model):
-    Date = models.DateField
-    Number = models.IntegerField
-    IdPerson = models.ForeignKey('Person', on_delete=models.CASCADE)
+    Data = models.DateTimeField()
+    Number = models.IntegerField()
+    Person = models.ForeignKey('Person', on_delete=models.CASCADE)
 
 
-class AnswerForm(models.Model):
-    IdTopic = models.ForeignKey('Topic', on_delete=models.CASCADE)
-    IdVarAnswer = models.ForeignKey('VarAnsForm', on_delete=models.CASCADE)
-    IdForm = models.ForeignKey('Form', on_delete=models.CASCADE)
+class Topic(models.Model):
+    Name = models.CharField(max_length=80)
+    Description = models.TextField()
+    Level = models.ForeignKey('Level', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Name
+
+class FormAnswer(models.Model):
+    Form = models.ForeignKey('Form', on_delete=models.CASCADE)
+    VarAnswer = models.ForeignKey('FormVarAnswer', on_delete=models.CASCADE)
+    Topic = models.ForeignKey('Topic', on_delete=models.CASCADE)
 
 
-class VarAnsForm(models.Model):
-    Name = models.CharField(max_length=50)
+
+
+class FormVarAnswer(models.Model):
+    Name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.Name
+
+class FinishTest(models.Model):
+    Data = models.DateTimeField()
+    Number = models.IntegerField()
+
+
+
+class Level(models.Model):
+    Name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.Name
+
+class Program(models.Model):
+    Name = models.CharField(max_length=80)
+    Level = models.ForeignKey('Level', on_delete=models.CASCADE)
+    Topic = models.ManyToManyField(Topic)
+    Person = models.ManyToManyField(Person)
+    FinishTest = models.OneToOneField(FinishTest, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return self.Name
+
+class FQuestion(models.Model):
+    Wording = models.TextField()
+
+
+class FVarAnswer(models.Model):
+    Name = models.CharField(max_length=80)
+    FQuestion = models.ForeignKey('FQuestion', on_delete=models.CASCADE)
+    Point = models.IntegerField()
+
+
+class FResult(models.Model):
+    Finished = models.BooleanField()
+
+
+class FTestAnswer(models.Model):
+    FinishTest = models.ForeignKey('FinishTest', on_delete=models.CASCADE)
+    FVarAnswer = models.ForeignKey('FVarAnswer', on_delete=models.CASCADE)
+    FQuestion = models.ForeignKey('FQuestion', on_delete=models.CASCADE)
+    FResult = models.ForeignKey('FResult', on_delete=models.CASCADE)
+
+
+
+
+
+

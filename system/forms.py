78b54696fcc59be2b4059d9e31d1f8ef6d7b0ef9
iamAnyBu <82719093+iamAnyBu@users.noreply.github.com>
2021-06-03 16:18:21 +0300

    Update forms.py

--- system/forms.py
+++ system/forms.py
@@ -1,6 +1,10 @@
+
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
 from django.contrib.auth.models import User
+from django.forms import ModelForm
+from .models import Client, FResult, FormVarAnswer
+
 
 #ФОРМЫ РЕГИСТРАЦИИ И ВХОДА (ОНИ ВСТРОЕНЫ В ДЖАНГО)
 class UserLoginForm(AuthenticationForm):
@@ -16,3 +20,22 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class ClientForm(ModelForm):
+    SecondName = forms.CharField(label='Имя')
+    FirstName = forms.CharField(label='Фамилия')
+    LastName = forms.CharField(label='Отчество')
+    Name = forms.CharField(label='Место работы')
+    Address = forms.CharField(label='Адрес работы')
+
+    class Meta:
+        model = Client
+        fields = ('SecondName', 'FirstName', 'LastName', 'Name', 'Address')
+
+
+class CheckForm(ModelForm):
+    Name = forms.CharField()
+
+    class Meta:
+        model = FormVarAnswer
+        fields = ('Name',)
